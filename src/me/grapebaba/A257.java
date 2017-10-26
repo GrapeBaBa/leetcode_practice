b3b5a71d2f0d22bedc59ae0f05c930dda9065054
grapebaba <281165273@qq.com>
2017-10-26 09:03:17 +0800

    A257

---
+++ src/me/grapebaba/A257.java
@@ -0,0 +1,41 @@
+package me.grapebaba;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+/**
+ * Created by grapebaba on 10/25/17.
+ */
+public class A257 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root != null) pathFromNode(root, "", res);
+        return res;
+    }
+
+    private void pathFromNode(TreeNode node, String path, List<String> res) {
+        if (node.left != null) {
+            pathFromNode(node.left, path + node.val + "->", res);
+        }
+        if (node.right != null) {
+            pathFromNode(node.right, path + node.val + "->", res);
+        }
+        if (node.left == null && node.right == null) {
+            res.add(path + node.val);
+        }
+    }
+}
